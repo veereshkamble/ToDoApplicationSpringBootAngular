3065c04335a67ae21d4366ce8ac1c10adb7b5467
veereshkamble <veereshkamble7@gmail.com>
2019-11-11 15:43:14 -0800

    Implementing logout to remove User Authentication Service - RouteGuardService, canActivate()

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
@@ -7,12 +7,15 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private hardCOdedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(
+    private hardCOdedAuthenticationService: HardcodedAuthenticationService,
+    private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if(this.hardCOdedAuthenticationService.isUserLoggedIn()) {
       return true;
     }
+    this.router.navigate(['login']);
     return false;
   }
 }
