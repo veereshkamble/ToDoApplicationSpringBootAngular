6b1734db15cddea60cbb8c1caa508104b9beb778
veereshkamble <veereshkamble7@gmail.com>
2019-11-22 23:44:20 -0800

    Implementing Update Todo feature in Angular frontend

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -15,7 +15,8 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoDataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -30,6 +31,7 @@ export class TodoComponent implements OnInit {
     this.todoService.updateTodo("veereshkamble", this.id, this.todo).subscribe(
       data => {
         console.log(data)
+        this.router.navigate['todos']
       }
     )
   }
