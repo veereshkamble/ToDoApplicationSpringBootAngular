30f369eebb0e78260710703291a9f6cda74f175d
veereshkamble <veereshkamble7@gmail.com>
2019-11-24 16:45:46 -0800

    Implementing new Todo feature in Angular Frontend

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -21,19 +21,26 @@ export class TodoComponent implements OnInit {
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(1, '', false, new Date())
-    this.todoService.retrieveTodo("veereshkamble", this.id).subscribe(
-      data => this.todo = data
-    )
+    this.todo = new Todo(this.id, '', false, new Date())
+
+    if(this.id != -1) {
+      this.todoService.retrieveTodo("veereshkamble", this.id).subscribe(
+        data => this.todo = data
+      )
+    }
   }
 
   saveTodo() {
-    this.todoService.updateTodo("veereshkamble", this.id, this.todo).subscribe(
-      data => {
-        console.log("In save Todo")
-        console.log(data)
-        this.router.navigate(['todos'])
-      }
-    )
+    if(this.id === -1) {
+
+    } else {
+      this.todoService.updateTodo("veereshkamble", this.id, this.todo).subscribe(
+        data => {
+          console.log("In save Todo")
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
+    }
   }
 }
