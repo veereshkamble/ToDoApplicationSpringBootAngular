2f96983914c76104d431fb42d7044b275efc6cde
veereshkamble <veereshkamble7@gmail.com>
2019-12-05 21:46:14 -0800

    Enhancing Angular welcome data service to use asic Auth

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
@@ -22,13 +22,22 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name) {
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader()
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    })
+
     console.log("Execute  Hello World Bean Service")
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/path-variable/${name}` );
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/path-variable/${name}`, {
+      headers: headers
+    } );
   }
 
   createBasicAuthenticationHttpHeader() {
     let username = 'veereshkamble'
     let password = 'password'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password)
+    return basicAuthHeaderString
   }
   // Access to XMLHttpRequest at 'http://localhost:8080/hello-world-bean/path-variable/veereshkamble' from origin 
   // 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
