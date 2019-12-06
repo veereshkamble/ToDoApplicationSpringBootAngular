359f200662e286ff422d538da30bd7e1709ac175
veereshkamble <veereshkamble7@gmail.com>
2019-12-05 21:34:43 -0800

    Enhancing Angular welcome data service to use asic Auth

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -23,6 +23,13 @@ export class WelcomeDataService {
 
   executeHelloWorldBeanServiceWithPathVariable(name) {
     console.log("Execute  Hello World Bean Service")
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/path-variable/${name}`);
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/path-variable/${name}` );
   }
+
+  createBasicAuthenticationHttpHeader() {
+    let username = 'veereshkamble'
+    let password = 'password'
+  }
+  // Access to XMLHttpRequest at 'http://localhost:8080/hello-world-bean/path-variable/veereshkamble' from origin 
+  // 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
 }
